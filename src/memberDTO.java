8d11dd0fa1ef190a45b8ecc60b7a7c217ebde2e2
Shin_BG <88640123+ShinBG@users.noreply.github.com>
2021-11-08 11:46:10 +0900

    Add files via upload

---
+++ src/memberDTO.java
@@ -0,0 +1,40 @@
+package DTO;
+
+public class memberDTO {
+int user_number;
+String id;
+String pw;
+String name;
+int phone;
+
+public void setUser_number(int user_number) {
+	this.user_number=user_number;
+}
+public int getUser_number() {
+	return user_number;
+}
+public void setId(String id) {
+	this.id=id;
+}
+public String getID() {
+	return id;
+}
+public void setPw(String pw) {
+	this.pw=pw;
+}
+public String getPw() {
+	return pw;
+}
+public void setName(String name) {
+	this.name=name;
+}
+public String getName() {
+	return name;
+}
+public void setPhone(int phone) {
+	this.phone=phone;
+}
+public int getPhone() {
+	return phone;
+}
+}
